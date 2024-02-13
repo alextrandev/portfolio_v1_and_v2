3e99f483dc7a720b6da6dc62a498499feea64f79
dk10119 <tranduclong092@gmail.com>
2024-02-13 09:09:54 +0200

    refactor codes for readability

--- pancake_shop/script.js
+++ pancake_shop/script.js
@@ -1,82 +1,76 @@
-let toppingExtra = document.querySelectorAll('input[type = "checkbox"]');
-let toppingExtraConfirm = [];
-let pancakeTypeConfirm = "";
-let customerName = "";
-let customerAddress = "";
-let customerNote = "";
+let toppingExtraArr = [];
+let toppings = [];
+let extras = [];
 let totalPrice = 5;
+const pancakeType = document.querySelector("#type");
+const toppingExtra = document.querySelectorAll('input[type = "checkbox"]');
+const formContainer = document.querySelector(".form-container");
+const confirmContainer = document.querySelector(".confirm-container");
+const thankYouContainer = document.querySelector(".thank-you-container");
+const toppingImage = document.querySelectorAll(".toppingImage");
 
-//to change the cake images and calculate price
-function changeOrder() {
-  let pancakeType = document.querySelector("#pancakeTypeConfirm");
-  let defaultImage = document.querySelector(".imageDefault");
-  let toppingImage = document.querySelectorAll(".toppingImage");
-  switch (document.querySelector("#type").value) {
-    case "5":
-      defaultImage.src = "images/pancakeClassic.webp";
-      totalPrice = 5;
-      pancakeTypeConfirm = pancakeType.textContent = "Classic";
-      break;
-    case "6":
-      defaultImage.src = "images/pancakeChocolate.webp";
-      totalPrice = 6;
-      pancakeTypeConfirm = pancakeType.textContent = "Chocolate";
-      break;
-    case "7":
-      defaultImage.src = "images/pancakeBlueberry.webp";
-      totalPrice = 7;
-      pancakeTypeConfirm = pancakeType.textContent = "Blueberry";
-      break;
-  }
-  toppingExtraConfirm = []; //reset the toppings array
-  //can do foreach or for of here but "for" give index which reused for img
+const changeTypes = () => {
+  const defaultImage = document.querySelector(".imageDefault");
+  defaultImage.src = pancakeType.selectedOptions[0].dataset.image;
+  totalPrice = parseInt(pancakeType.value);
+};
+
+const changeToppingsExtras = () => {
+  toppings = [];
+  extras = [];
   for (let i = 0; i < toppingExtra.length; i++) {
     if (toppingExtra[i].checked) {
-      totalPrice += Number(toppingExtra[i].value);
       toppingImage[i].style.visibility = "visible";
-      toppingExtraConfirm.push(toppingExtra[i].id);
+      totalPrice += parseInt(toppingExtra[i].value);
+      toppingExtra[i].dataset.category == "toppings"
+        ? toppings.push(toppingExtra[i].dataset.name)
+        : extras.push(toppingExtra[i].dataset.name);
     } else {
       toppingImage[i].style.visibility = "hidden";
     }
   }
-  document
-    .querySelectorAll(".totalPrice")
-    .forEach((price) => (price.textContent = totalPrice.toFixed(2)));
+};
+
+function changeOrder() {
+  const priceDisplay = document.querySelectorAll(".totalPrice");
+  changeTypes();
+  changeToppingsExtras();
+  priceDisplay.forEach((price) => (price.textContent = totalPrice.toFixed(2)));
 }
 
 //move to the confirm tab on button click, display the selections
 function confirmCake() {
-  document.querySelector(".form-container").style.display = "none";
-  document.querySelector(".confirm-container").style.display = "flex";
-  let toppingExtraDisplay = toppingExtraConfirm.map((item) => {
-    item = (item[0].toUpperCase() + item.slice(1)).split(/(?=[A-Z])/).join(" ");
-    return item;
-  });
-  let toppingExtra = document.querySelector("#toppingExtraConfirm");
-  toppingExtra.textContent = toppingExtraDisplay.join(", ");
-  toppingExtraConfirm == [] ? (toppingExtra.textContent = "None") : null;
+  const pancakeTypeDisplay = document.querySelector("#pancakeTypeConfirm");
+  const toppingsDisplay = document.querySelector("#toppingsDisplay");
+  const extrasDisplay = document.querySelector("#extrasDisplay");
+  formContainer.style.display = "none";
+  confirmContainer.style.display = "flex";
+  pancakeTypeDisplay.textContent = pancakeType.selectedOptions[0].dataset.name;
+  toppings.length == 0
+    ? (toppingsDisplay.textContent = "None")
+    : (toppingsDisplay.textContent = toppings.join(", "));
+  extras.length == 0
+    ? (extrasDisplay.textContent = "None")
+    : (extrasDisplay.textContent = extras.join(", "));
 }
 
-//just to move back to cake customize
+//to move back to cake customize
 function backToOrder() {
-  document.querySelector(".confirm-container").style.display = "none";
-  document.querySelector(".form-container").style.display = "flex";
+  confirmContainer.style.display = "none";
+  formContainer.style.display = "flex";
+  thankYouContainer.style.display = "none";
 }
 
 //save the user info and display a thanks card
 function confirmOrder() {
-  customerName = document.querySelector("#customerName").value;
-  customerAddress = document.querySelector("#customerAddress").value;
-  customerNote = document.querySelector("#customerNote").value;
-  document.querySelector(".confirm-container").style.display = "none";
-  document.querySelector(".thank-you-container").style.display = "flex";
+  let customerName = document.querySelector("#customerName").value;
+  let customerAddress = document.querySelector("#customerAddress").value;
+  let customerNote = document.querySelector("#customerNote").value;
+  confirmContainer.style.display = "none";
+  thankYouContainer.style.display = "flex";
 }
 
-//add trigger to buttons, checkboxes, inputs
+//trigger to buttons, checkboxes, inputs
+formContainer.addEventListener("change", changeOrder);
 document.querySelector("#confirmButton").addEventListener("click", confirmCake);
-document.querySelector("#backToOrder").addEventListener("click", backToOrder);
-document.querySelector("#type").addEventListener("change", changeOrder);
-toppingExtra.forEach((checkbox) =>
-  checkbox.addEventListener("change", changeOrder)
-);
 document.querySelector("#orderButton").addEventListener("click", confirmOrder);
