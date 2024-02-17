52484a3c5541abc78998b197a81daf5a36f9b791
dk10119 <tranduclong092@gmail.com>
2024-02-17 18:50:39 +0200

    fix show all countries button. and refactor some code blocks for readability

--- country_info/main.js
+++ country_info/main.js
@@ -1,61 +1,98 @@
-async function fetchAsync() {
+const sortAlphabet = (data) =>
+  data.sort((a, b) => {
+    if (a.name.common > b.name.common) return 1;
+    else if (a.name.common < b.name.common) return -1;
+    else return 0;
+  });
+
+function createRegionSection(region) {
+  const section = document.createElement("section");
+  section.className = "regionSection";
+  section.id = region;
+  const h2 = document.createElement("h2");
+  h2.textContent = region;
+  section.appendChild(h2);
+  const ul = document.createElement("ul");
+  ul.className = "gridContainer";
+  ul.id = `regionList${region}`;
+  section.appendChild(ul);
+  section.style.display = "none";
+  document.querySelector("#regionsMain").appendChild(section);
+}
+
+function createCountryCard(country, region) {
+  let li = document.createElement("li");
+  li.className = "gridItem";
+  let div = document.createElement("div");
+  div.className = "flagContainer";
+  div.addEventListener("click", () => fetchCountryOnClick(country.cca3));
+  let img = document.createElement("img");
+  img.src = country.flags.svg;
+  div.appendChild(img);
+  let h3 = document.createElement("h3");
+  h3.textContent = country.name.common;
+  div.appendChild(h3);
+  li.appendChild(div);
+  document.querySelector(`#regionList${region}`).appendChild(li);
+}
+
+function createRegionSelectButton(region) {
+  const regionH2 = document.createElement("h2");
+  regionH2.className = "region_select_button";
+  regionH2.id = `${region}Section`;
+  regionH2.textContent = region;
+  const regionSelect = document.querySelector(".region_select");
+  regionH2.addEventListener("click", () => changeRegion(regionH2.textContent));
+  regionSelect.appendChild(regionH2);
+}
+
+function changeRegion(region) {
+  const regionSection = document.querySelectorAll(".regionSection");
+  regionSection.forEach((section) => (section.style.display = "none"));
+  const thisRegion = document.querySelector(`#${region}`);
+  thisRegion.style.display = "block";
+}
+
+async function fetchAllCountry() {
+  const response = await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,flags,cca3"
+  );
+  const data = await response.json();
+  const sortedData = sortAlphabet(data);
+  let region = "All";
+  createRegionSelectButton(region);
+  createRegionSection(region);
+  document.querySelector("#All").style.display = "block";
+  for (country of sortedData) {
+    createCountryCard(country, region);
+  }
+}
+
+async function fetchCountriesByRegion() {
   const response = await fetch(
     "https://restcountries.com/v3.1/all?fields=name,flags,cca3,region"
   );
   const data = await response.json();
   const regions = new Set(data.map((country) => country.region));
   for (region of regions) {
-    const section = document.createElement("section");
-    section.className = "regionSection";
-    section.id = region;
-    const h2 = document.createElement("h2");
-    h2.textContent = region;
-    section.appendChild(h2);
-    const ul = document.createElement("ul");
-    ul.className = "gridContainer";
-    section.appendChild(ul);
-    document.querySelector("#regionsMain").appendChild(section);
-    const sortedData = data
-      .filter((country) => country.region == region)
-      .sort((a, b) => {
-        if (a.name.common > b.name.common) return 1;
-        else if (a.name.common < b.name.common) return -1;
-        else return 0;
-      });
-    sortedData.forEach((country) => {
-      let li = document.createElement("li");
-      li.className = "gridItem";
-      let div = document.createElement("div");
-      div.className = "flagContainer";
-      div.addEventListener("click", () => fetchAsyncOnClick(country.cca3));
-      let img = document.createElement("img");
-      img.src = country.flags.svg;
-      div.appendChild(img);
-      let h3 = document.createElement("h3");
-      h3.textContent = country.name.common;
-      div.appendChild(h3);
-      li.appendChild(div);
-      ul.appendChild(li);
-    });
-    const regionH2 = document.createElement("h2");
-    regionH2.className = "region_select_button";
-    regionH2.id = `${region}Section`;
-    regionH2.textContent = region;
-    const regionSelect = document.querySelector(".region_select");
-    regionH2.addEventListener("click", () =>
-      changeRegion(regionH2.textContent)
+    createRegionSection(region);
+    const filteredData = sortAlphabet(
+      data.filter((country) => country.region == region)
     );
-    regionSelect.appendChild(regionH2);
+    filteredData.forEach((country) => {
+      createCountryCard(country, region);
+    });
+    createRegionSelectButton(region);
   }
 }
-fetchAsync(); //run the function of page load
 
-async function fetchAsyncOnClick(countryCode) {
+async function fetchCountryOnClick(countryCode) {
   let response = await fetch(
     "https://restcountries.com/v3.1/all?fields=name,cca3,currencies,capital,region,subregion,languages,area,maps,population,flags,coatOfArms"
   );
   let data = await response.json();
   let selectedCountry = data.find((country) => country.cca3 === countryCode);
+  // console.log(selectedCountry);
   document.querySelector("#countryName").textContent =
     document.querySelector("#countryNameCoa").textContent =
     document.querySelector("#countryNameFact").textContent =
@@ -85,11 +122,7 @@ async function fetchAsyncOnClick(countryCode) {
   document.querySelector("#countryContainer").scrollIntoView();
 }
 
-function changeRegion(region) {
-  const regionSection = document.querySelectorAll(".regionSection");
-  regionSection.forEach((section) => (section.style.display = "none"));
-  const thisRegion = document.querySelector(`#${region}`);
-  thisRegion.style.display = "block";
-}
+fetchAllCountry(); //load the all countries section on page load
+fetchCountriesByRegion(); //load the region section on page load
 
 //need to work on click disapearing, multiple name ID, map, currencies(maybe use foreach), info not available condition. commenting on codes. animation, hover, sorting, region tabs, closing or back button on countryContainer.
