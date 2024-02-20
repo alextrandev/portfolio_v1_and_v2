45b367ff49aa5d819c4c583431f4717b0a040ce0
dk10119 <tranduclong092@gmail.com>
2024-02-20 18:13:37 +0200

    refector some code blocks for readability, add some commend on codes

--- country_info/main.js
+++ country_info/main.js
@@ -4,9 +4,10 @@ const sortAlphabet = (data) =>
     else if (a.name.common < b.name.common) return -1;
     else return 0;
   });
+const find = (name) => document.querySelector(name);
+const findAll = (name) => document.querySelectorAll(name);
 
-const id = (name) => document.querySelector(`#${name}`);
-
+//create a section for each region for easy sorting
 function createRegionSection(region) {
   const section = document.createElement("section");
   section.className = "regionSection";
@@ -16,9 +17,10 @@ function createRegionSection(region) {
   ul.id = `regionList${region}`;
   section.appendChild(ul);
   section.style.display = "none";
-  id("regionsMain").appendChild(section);
+  find("#regionsMain").appendChild(section);
 }
 
+//create a card for country selection with flag and name
 function createCountryCard(country, region) {
   let li = document.createElement("li");
   li.className = "gridItem";
@@ -32,31 +34,31 @@ function createCountryCard(country, region) {
   h3.textContent = country.name.common;
   div.appendChild(h3);
   li.appendChild(div);
-  document.querySelector(`#regionList${region}`).appendChild(li);
+  find(`#regionList${region}`).appendChild(li);
 }
 
+//add a sorting button for each region
 function createRegionSelectButton(region) {
   const regionH2 = document.createElement("h2");
   regionH2.className = "region_select_button";
   regionH2.id = `${region}Section`;
   regionH2.textContent = region;
-  const regionSelect = document.querySelector(".region_select");
   regionH2.addEventListener("click", () => changeRegion(regionH2.textContent));
-  regionSelect.appendChild(regionH2);
+  find(".region_select").appendChild(regionH2);
 }
 
+//show the selected region section and hide others
 function changeRegion(region) {
-  const regionSection = document.querySelectorAll(".regionSection");
+  const regionSection = findAll(".regionSection");
   regionSection.forEach((section) => (section.style.display = "none"));
-  const thisRegion = document.querySelector(`#${region}`);
-  thisRegion.style.display = "block";
-  const allRegionButton = document.querySelectorAll(".region_select_button");
+  find(`#${region}`).style.display = "block";
+  const allRegionButton = findAll(".region_select_button");
   allRegionButton.forEach((button) => button.style.removeProperty("filter"));
-  const thisRegionButton = document.querySelector(`#${region}Section`);
-  thisRegionButton.style.filter = "drop-shadow(0px 0px 2px #3E92CC)";
+  find(`#${region}Section`).style.filter = "drop-shadow(0px 0px 2px #3E92CC)";
 }
 
-async function fetchAllCountry() {
+//will run at page load and load all country cards
+async function fetchAllCountries() {
   const response = await fetch(
     "https://restcountries.com/v3.1/all?fields=name,flags,cca3"
   );
@@ -65,12 +67,13 @@ async function fetchAllCountry() {
   let region = "All";
   createRegionSelectButton(region);
   createRegionSection(region);
-  document.querySelector("#All").style.display = "block";
+  find("#All").style.display = "block";
   for (country of sortedData) {
     createCountryCard(country, region);
   }
 }
 
+//run after the previous function and sort country cards based on region
 async function fetchCountriesByRegion() {
   const response = await fetch(
     "https://restcountries.com/v3.1/all?fields=name,flags,cca3,region"
@@ -89,48 +92,38 @@ async function fetchCountriesByRegion() {
   }
 }
 
+//run when a country is selected. load all infos to their placeholder.
 async function fetchCountryOnClick(countryCode) {
   let response = await fetch(
     "https://restcountries.com/v3.1/all?fields=name,cca3,currencies,capital,region,subregion,languages,area,maps,population,flags,coatOfArms"
   );
   let data = await response.json();
   let country = data.find((country) => country.cca3 === countryCode);
-  // console.log(country);
-  document.querySelector("#countryName").textContent =
-    document.querySelector("#countryNameCoa").textContent =
-    document.querySelector("#countryNameFact").textContent =
-    document.querySelector("#countryNameMap").textContent =
-      country.name.common;
-  document.querySelector("#officialName").textContent = country.name.official;
-  document.querySelector("#flagAlt").textContent = document.querySelector(
-    "#flag"
-  ).alt = country.flags.alt;
-  // document.querySelector("#flag").src = country.flags.svg;
-  id("flag").src = country.flags.svg;
-  document.querySelector("#coa").src = country.coatOfArms.svg;
-  document.querySelector("#cca3").textContent = country.cca3;
-  document.querySelector("#area").textContent = country.area;
-  document.querySelector("#capital").textContent = country.capital;
-  document.querySelector("#population").textContent = country.population;
-  document.querySelector("#region").textContent = country.region;
-  document.querySelector("#language").textContent = Object.values(
-    country.languages
-  ).join(", ");
-  document.querySelector("#subregion").textContent = country.subregion;
+  findAll(".countryName").forEach((i) => (i.textContent = country.name.common));
+  find("#officialName").textContent = country.name.official;
+  find("#flagAlt").textContent = find("#flag").alt = country.flags.alt;
+  find("#flag").src = country.flags.svg;
+  find("#coa").src = country.coatOfArms.svg;
+  find("#cca3").textContent = country.cca3;
+  find("#area").textContent = country.area;
+  find("#capital").textContent = country.capital;
+  find("#population").textContent = country.population;
+  find("#region").textContent = country.region;
+  find("#language").textContent = Object.values(country.languages).join(", ");
+  find("#subregion").textContent = country.subregion;
   let currencyString = "";
-  const currencyObj = country.currencies;
-  for (code of Object.keys(currencyObj)) {
-    currencyString += `${currencyObj[code].name} (${code} | ${currencyObj[code].symbol}) `;
+  for (code of Object.keys(country.currencies)) {
+    currencyString += `${country.currencies[code].name} (${code} | ${country.currencies[code].symbol}) `;
   }
-  document.querySelector("#currency").textContent = currencyString;
-  document.querySelector("#countryContainer").style.display = null;
-  document.querySelector("#countryContainer").scrollIntoView();
+  find("#currency").textContent = currencyString;
+  find("#countryContainer").style.display = null;
+  find("#countryContainer").scrollIntoView();
 }
 
-const closeCountryContainer = () =>
-  (document.querySelector("#countryContainer").style.display = "none");
+//a button function to close the info card
+const closeContainer = () => (find("#countryContainer").style.display = "none");
 
-fetchAllCountry(); //load the all countries section on page load
+fetchAllCountries(); //load the all countries section on page load
 fetchCountriesByRegion(); //load the region section on page load
 
-//map, info not available condition. commenting on codes. animation, hover, closing or back button on countryContainer.
+//nee to fix map not display, info not available condition. commenting on codes, add more infos.
