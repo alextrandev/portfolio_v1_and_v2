5fe1a8eaf5cd38239147092d7f2736b9fa47e28c
dk10119 <tranduclong092@gmail.com>
2024-03-04 13:40:00 +0200

    add scroll top button and sscroll function

--- CSS_tricks/main.js
+++ CSS_tricks/main.js
@@ -1,8 +1,27 @@
 const backToTopButton = document.querySelector("#back_to_top_button");
+const header = document.querySelector("header");
 
-const getToTop = () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+backToTopButton.addEventListener("click", () => {
+  document.body.scrollTop = 0; // for safari
+  document.documentElement.scrollTop = 0; // chrome, FF and others
+  header.classList.toggle("bg");
+});
+
+window.onscroll = function () {
+  document.body.scrollTop > 200 || document.documentElement.scrollTop > 200 ? (backToTopButton.style.display = "block") : (backToTopButton.style.display = "none");
+  // if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) header.style.backgroundColor = "#b2d5fb";
+  // if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) header.style.backgroundColor = "#aeffae";
+  // if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0) header.style.backgroundColor = "white";
+
+  // switch (document.body.scrollTop || document.documentElement.scrollTop) {
+  //   case 0:
+  //     header.style.backgroundColor = "white";
+  //     break;
+  //   case >1:
+  //     header.style.backgroundColor = "#b2d5fb";
+  //     break;
+  //   case 200:
+  //     header.style.backgroundColor = "aeffae";
+  //     break;
+  // }
 };
-
-backToTopButton.addEventListener("click", getToTop);
